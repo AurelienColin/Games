1a17754a34292732302b25bcdb482d544c382a52
Rignak <aureliencolin@hotmail.com>
2018-02-27 13:29:07 +0100

    Add support for multiple routes in VN, update the devTool accordingly (and resolve a old bug of file location)

--- VN/launcher.py
+++ VN/launcher.py
@@ -1,10 +1,12 @@
+KEYDOWN, K_RETURN, QUIT, K_UP, K_DOWN = 0,0,0,0,0
+
 from os.path import join
 import pygame
 import pyganim
 from pygame.locals import *
 import sys
 
-screenDim = (870,870)
+screenDim = (870,500)
 
 def Launcher():
     pygame.init()
@@ -33,6 +35,7 @@ def FormatText(text, l):
 
 def VNLoop(screen, lines):
     on_screen = {}
+    choice = False
     mainClock = pygame.time.Clock()
     for line in lines:
         change = False
@@ -67,6 +70,12 @@ def VNLoop(screen, lines):
                 music.stop()
             elif line[0] == 'sound':
                 pygame.mixer.Sound(join('..', 'res', 'sound', line[1])).play()
+            elif line[0] == 'choice':
+                choices = ' '.join(line[1:])
+                choice = ChoiceLoop(screen, choices)
+            elif line[0] == str(choice):
+                choice = line[1]
+                break
         while change == False:
             screen.refresh()
             mainClock.tick(30)
@@ -80,8 +89,43 @@ def VNLoop(screen, lines):
                         [screen.RemoveObject(index) for index in current_dialog]
     [screen.RemoveObject(i) for i in current_dialog + list(on_screen.values())]
     music.stop()
-    pygame.quit()
+    if choice:
+        fullname = join('..', 'res', 'script', choice)
+        file = open(fullname)
+        lines = file.readlines()
+        file.close()
+        VNLoop(screen, lines)
+    else:
+        pygame.quit()
 
+def ChoiceLoop(screen, choices):
+    frame = ['TextBox_Large.png']
+    pos = [(int((screen.size[0]-300)/2),screen.size[1]-100)]
+    choices = choices.split('&')
+    box = TextBox(frame, choices, [(300,100)], [(15, 10), (15, 30)], pos, size=17)
+    screen.menuIndex = screen.AddTextBox(box)
+    
+    alpha = 80
+    color = (0,0,0)
+    size, pos = ObjToCoord(screen.objects[screen.menuIndex[1]])
+    s = Highlight(size, alpha, color, pos)
+    selectId = screen.AddHighlight(s)
+    
+    select = 1
+    mainClock = pygame.time.Clock()
+    while True:
+        screen.refresh()
+        mainClock.tick(30)
+        for event in pygame.event.get():
+            if event.type == QUIT:  # The game is closed
+                pygame.quit()
+                sys.exit()
+            elif event.type == KEYDOWN:
+                if event.key == K_RETURN:
+                    screen.QuitMenu(selectId)
+                    return select
+                if event.key in (K_UP, K_DOWN):
+                    select, selectId = screen.MenuNavigation(event.key, select, selectId)
 
 class TextBox():
     def __init__(self, files, texts, dim, text_pos, pos, size=20, color={'default':(0,0,0)}):
@@ -107,6 +151,7 @@ class TextBox():
         self.imgs = False
         self.pos = pos
 
+        print(screenDim, '|', self.size[0], '|', self.pos[0])
         decal = [min(0, screenDim[0]-self.size[0][0]-self.pos[0][0]),
                  min(0, screenDim[1]-self.size[0][1]-self.pos[0][1])]
         for i, xy in enumerate(self.pos):
@@ -126,6 +171,14 @@ class Text():
         self.text = text
         self.string = font.render(text, True, color)
         self.pixel = pixel
+
+class Highlight():
+    def __init__(self, size, alpha, color, pos):
+        s = pygame.Surface(size)
+        s.fill(color)
+        s.set_alpha(alpha)
+        self.content = s
+        self.pixel = pos
 
 class Screen():
     def __init__(self, size):
@@ -158,9 +211,40 @@ class Screen():
         self.objects[prec].append(index)
         return index
 
+    def AddHighlight(self, s, priority=True):
+        if priority:
+            self.objects.append([s.content, (s.pixel[0], s.pixel[1]), 'highlight'])
+        else:
+            self.objects.append([s.content, (s.pixel[0], s.pixel[1]), 'sprite'])
+        return len(self.objects)-1
+        
+    def MenuNavigation(self, key, select, selectId):
+        alpha = 80
+        color = (0,0,0)
+        self.RemoveObject(selectId)
+        pos = [(self.size[0]-128, self.size[1]-2*100)]
+        if key == K_DOWN:
+            select = (select+1)%len(self.menuIndex)
+            if select == 0:
+                select +=1
+        elif key == K_UP:
+            select = (select-1)%len(self.menuIndex)
+            if select == 0:
+                select = len(self.menuIndex)-1
+        size, pos = ObjToCoord(self.objects[self.menuIndex[select]])
+        s =Highlight(size, alpha, color, pos)
+        selectId = self.AddHighlight(s)
+        return select, selectId
+        
+    def QuitMenu(self, selectId):
+        for i in self.menuIndex:
+            self.RemoveObject(i)
+        self.RemoveObject(selectId)
+        
     def refresh(self):
         sprites = [obj[:2] for obj in self.objects if obj and obj[2]=='sprite']
         box = [obj[:4] for obj in self.objects if obj and obj[2]=='box']
+        highlight = [obj[:2] for obj in self.objects if obj and obj[2] == 'highlight']
         [self.display.blit(ele, pos) for ele, pos in sprites]
         for ele, pos, t, index in box:
             for i in index:
@@ -170,8 +254,27 @@ class Screen():
                 elif type(ele2) != pyganim.PygAnimation :
                     for j in range(len(ele2.box)):
                         self.display.blit(ele2.box[j], ele2.pos[j])
+        [self.display.blit(ele, pos) for ele, pos in highlight]
 
         pygame.display.update()
 
+def ObjToCoord(obj):
+    """From an object, retourn it's coordinates
+
+    Input:
+    obj - object: tuple of:
+        [0] : the actual object
+        [1] : the coordinates (a tuple of int)
+        [2] ; its type (sprite, map, or anything else), a string
+
+    output:
+    rect.height - int: height of the object
+    rect.width - int: width of the object
+    posX - int: px pos of the object (up left)
+    pox_y - int: px pos of the object (up left)"""
+    rect = obj[0].get_rect()
+    posX, posY = obj[1]
+    return rect.size, obj[1]
+    
 if __name__ == '__main__':
     Launcher()
